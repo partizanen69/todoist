2ce9d90ea44b9f347f7f2c8a6437d21d96a0bb7c
partizanen69 <oleksii.ablitsov@gmail.com>
2018-08-02 12:38:33 +0300

    a

---
+++ src/pages/Account/Styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export default styled.div`
+	.wrapper {
+		display: flex;
+		justify-content: flex-start;
+	}
+
+	.form {
+		display: flex;
+		justify-content: flex-start;
+
+		& form {
+			width: 210px;
+		}
+	}
+`;
